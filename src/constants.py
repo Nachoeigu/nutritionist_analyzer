d4a4b1bb845a9db3b8ed31fe45c7a5c965da18a2
Nachoeigu <ignacio.eiguren@gmail.com>
2023-12-01 14:19:37 +0100

    .

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,6 @@
 IMAGE_EXTENSIONS = {'.jpg', '.jpeg', '.png', '.gif', '.bmp'}
 
-PROMPT_IMG_RECOGNIZER = "Extract ingredients/nutritional info from the input. Avoid introduction, go beyond the desired output. If is unclear or not visible, answer only 'None'. Otherwise, uniquely list ingedients and/or nutritional info"
+PROMPT_IMG_RECOGNIZER = "Extract ingredients/nutritional info. Avoid introduction, go beyond the desired output. If is unclear or not visible, answer only 'None'. Otherwise, list desired info"
 
 HEADERS_IMG_RECOGNIZER_STRUCTURE = {
     "Content-Type": "application/json",
